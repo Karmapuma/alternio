5979be78bea6a014ec70799e8905f30d9c0fddd6
Karmapuma <kar.aka@yahoo.com>
2025-05-13 11:19:37 +0200

    TooMuch

--- src/Controller/PublicLogementsController.php
+++ src/Controller/PublicLogementsController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Logements;
+use App\Form\LogementsType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/logements')]
 class PublicLogementsController extends AbstractController
@@ -19,6 +21,34 @@ class PublicLogementsController extends AbstractController
         return $this->render('logements/index.html.twig', [
             'logements' => $logements,
             'is_admin' => $this->isGranted('ROLE_ADMIN'),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_public_logements_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN') && !$this->isGranted('ROLE_OWNER')) {
+            throw $this->createAccessDeniedException('Vous n\'avez pas les droits nécessaires pour ajouter un logement.');
+        }
+
+        $logement = new Logements();
+        $form = $this->createForm(LogementsType::class, $logement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $logement->setDateAjout(new \DateTime());
+            $logement->setOwner($this->getUser());
+            
+            $entityManager->persist($logement);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le logement a été créé avec succès.');
+            return $this->redirectToRoute('app_public_logements_index');
+        }
+
+        return $this->render('logements/new.html.twig', [
+            'logement' => $logement,
+            'form' => $form,
         ]);
     }
 
